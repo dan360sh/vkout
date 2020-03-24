8e2e6f9c09ae4512264a233350dfd7aec9ef1ef4
dan360sh <swqazxcd@gmail.com>
2020-03-24 13:20:40 +0300

    save3

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const http = require('https');
+const server = require('http').createServer();
 const fs = require('fs');
+const http = require('https');
+const nodeStatic = require('node-static');
+const file = new nodeStatic.Server('.', {
+  cache: 0
+});
+server.listen(80, () => console.log("сервер запущен"));
+server.on('request', (req, res) => {
+  file.serve(req, res);
+});
