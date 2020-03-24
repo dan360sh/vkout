8bcbee04f05404762416497cc7afde8f67e191ad
dan360sh <swqazxcd@gmail.com>
2020-03-24 16:32:27 +0300

    6

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const file = new nodeStatic.Server('.', {
   cache: 0
 });
 
-let str = '';
-let client_id = '6270779';
-let client_secret = 'OZggJ070SyQVXMDaRRnJ';
-let redirect_uri = 'http://danilshitov.ru/';
+var str = '';
+var client_id = '6270779';
+var client_secret = 'OZggJ070SyQVXMDaRRnJ';
+var redirect_uri = 'http://danilshitov.ru/';
 server.listen(80, () => console.log("сервер запущен"));
 server.on('request', (req, res) => {
 	console.log(req.url);
 	if(req.method=='GET'){
-		let code = req.url.split('=')[1];
+		var code = req.url.split('=')[1];
 		str = 'https://oauth.vk.com/access_token?client_id='+client_id+'&client_secret='+client_secret+'&redirect_uri='+redirect_uri+'&code='+code;
 		console.log(str);
 		get();
@@ -27,7 +27,7 @@ function get (){
 		 const { statusCode } = res;
 	     const contentType = res.headers['content-type'];
 	   	 res.setEncoding('utf-8');
-	   	 let rawData = '';
+	   	 var rawData = '';
 	   	 res.on('data', (chunk) => { rawData += chunk; });
      	 res.on('end', () => {
      	 	console.log(rawData);
