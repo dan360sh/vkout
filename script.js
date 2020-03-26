157aa362cbca870f3ebe20fb7cd984fd06631bd2
dan360sh <swqazxcd@gmail.com>
2020-03-26 18:00:40 +0300

    dffd

--- script.js
+++ script.js
@@ -1,12 +1,92 @@
 const server = require('http').createServer();
 const fs = require('fs');
 const https = require('https');
+const sqlite3 = require('sqlite3').verbose();
 const cheerio = require('cheerio');
 var mime = require('mime');
 const nodeStatic = require('node-static');
 const file = new nodeStatic.Server('.', {
   cache: 0
 });
+function cookie_parse(cookie){
+	let m = {};
+	let c = cookie.split('; ');
+	for(var i in c){
+		m[c[i].split('=')[0]] = c[i].split('=')[1];
+	}
+	return m;
+}
+function parsedom(file,data){
+	var h1 = data.first_name + ' ' + data.last_name;
+	const $ = cheerio.load(file);
+	$('.h1').text(h1);
+	$('.a1').remove();
+	$('.block').html("<button class='btn2'>выйти</button>");
+	//mass_header['Set-Cookie'] = 'login=dan12';
+	return $.html();
+}
+async function sqlite(sql){
+	var base_open = [];
+  return new Promise((resolve,reject)=>{
+    let db = new sqlite3.Database('magic.db', (err) => {
+  			if (err) {
+  	            resolve(err);  	            
+  	        }
+  	});
+  	db.all(sql, [], (err, rows) => {
+  	  if (err) {
+  	    resolve(err);
+  	   
+  	  }else{
+  		  try{
+  			  rows.forEach((row) => {
+  			    base_open.push(row);  
+  			  });
+  			  resolve(base_open);
+  		  }catch(e){
+  		  	  resolve(rows);
+  		  }
+  	  }
+
+  	});
+ });
+    db.close();      
+}
+async function mainsql(){
+    var str = await sqlite(`CREATE TABLE user(
+     id TEXT NOT NULL,
+ 	 password TEXT NOT NULL,
+     data TEXT NOT NULL)`);
+    console.log(str)
+}
+async function user_in(id,password,data){
+	var str = await sqlite( `INSERT INTO user(id,password,data) 
+	VALUES('`+id+`','`+password+`','`+data+`')`);
+	return str;
+}
+async function user_select(login1){
+	console.log(login1);
+	var str = await sqlite( `SELECT*FROM user WHERE id = '`+login1+`'`);
+	return str;
+}
+async function main(){
+	 let data = {
+      id: 140104825,
+      first_name: 'Данил',
+      last_name: 'Шитов',
+      is_closed: false,
+      can_access_closed: true,
+      bdate: '29.5.1997'
+     }
+	//await user_in('danil3','123',JSON.stringify(data));
+	var s = await user_select('danil3');
+	console.log(JSON.parse(s[0].data).id);
+}
+ 
+ //main();
+
+//mainsql();
+
 
 var str = '';
 var client_id = '6270779';
@@ -16,7 +96,7 @@ server.listen(80, () => console.log("сервер запущен"));
 server.on('request', async function(req, res){
 	let head = 'text/html';
 	let mass_header = {};
-	let file = ''
+	let file = '';
 	if(req.url=='/'){
 		head = 'text/html';
 		file = 'index.html';
@@ -30,6 +110,24 @@ server.on('request', async function(req, res){
 	}catch(e){
 		var index = fs.readFileSync('index.html');
 	}
+
+	let cookie = cookie_parse(req.headers.cookie);
+	if(cookie['login']){
+		let l = await user_select(cookie['login']);
+		if(l.errno==undefined){
+			console.log('index2');
+			if(cookie['password']){
+				console.log('index1');
+				if(l[0].password==cookie['password']){
+					let p = JSON.parse(l[0].data);
+					index = parsedom(index,JSON.parse(l[0].data));
+					mass_header['Set-Cookie'] = 'login='+p.id+'; password='+p.id+5;
+					console.log('index');
+				}
+			}
+		}
+	 }
+	 console.log(req.headers.cookie)
 	 if(req.headers['cache-control']){
 	  	res.writeHead(200,mass_header);
 	  	res.end(index);
@@ -39,6 +137,7 @@ server.on('request', async function(req, res){
 	  	res.end(index);
 	  	return;
 	  }
+
 	 console.log(req.url);
 
 		if(req.url.indexOf("?")!=-1){
@@ -54,7 +153,7 @@ server.on('request', async function(req, res){
 			$('.h1').text(h1);
 			$('.a1').remove();
 			$('.block').html("<button class='btn2'>выйти</button>");
-			mass_header['Set-Cookie'] = 'login='+ h1;
+			mass_header['Set-Cookie'] = 'login='+user2.response[0].id+'; password='+user2.response[0]+5;
 			index = $.html();
 			console.log(index);
 		}
